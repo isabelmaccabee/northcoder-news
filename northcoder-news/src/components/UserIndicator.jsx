4a175762d3f9ad01c57b5b6a21c0c79c8f60d82b
isabelmaccabee <isabel.maccabee@gmail.com>
2018-12-12 09:25:22 +0000

    Added avatar images

--- northcoder-news/src/components/UserIndicator.jsx
+++ northcoder-news/src/components/UserIndicator.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import "../css/UserIndicator.css";
 
 const UserIndicator = ({ user }) => {
   const { username, avatar_url } = user;
   return (
     <div className="userInd">
       <p>You are signed in as {username}</p>
-      <img src={`${avatar_url}`} alt={`avatar for user ${username}`} />
+      <img
+        src={`${avatar_url}`}
+        alt={`avatar for user ${username}`}
+        className="userAvatar"
+      />
     </div>
   );
 };
